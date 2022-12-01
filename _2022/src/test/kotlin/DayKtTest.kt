ed8f767ff2d745f6bc0152d764bc656d80a3000d
Aksel <ahsorbye@gmail.com>
2022-12-01 20:58:16 +0100

    Fix all on new local machine

---
+++ _2022/src/test/kotlin/DayKtTest.kt
@@ -0,0 +1,31 @@
+import junit.framework.TestCase
+
+class DayKtTest : TestCase() {
+    val input = """ 
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+
+    """.trimIndent().lines()
+
+    fun testSolve1() {
+        val ans = solve(input)[0]
+        assertEquals(24000, ans)
+    }
+
+    fun testSolve2() {
+        //val ans = solve2(input)
+        //assertEquals(-1,ans)
+    }
+}
