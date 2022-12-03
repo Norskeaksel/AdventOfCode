80cd9d2a819ce67c35a869ba3c1f00c6df4ab39b
aksels <akselhso@stud.ntnu.no>
2022-12-03 14:32:53 +0100

    Start cleanup of 2021 files

---
+++ _2021/UnrefractoredTests/Day9Test.kt
@@ -0,0 +1,13 @@
+/*import junit.framework.TestCase
+import org.junit.Test
+
+class Day9Test : TestCase() {
+
+    @Test
+    fun testD9p1() {
+        val testPath = "src/testInputFiles/day9.txt"
+        val testinput = readFileLns(testPath)
+        val expected = 15
+        assertEquals(expected, part1(testinput))
+    }
+}*/
