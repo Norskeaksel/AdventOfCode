61e5d17abbdfd0b34fdf043c18b272df20850056
Norskeaksel <ahsorbye@gmail.com>
2023-12-09 21:27:45 +0100

    Solve day 8

---
+++ _2023/src/days/Day8.kt
@@ -0,0 +1,79 @@
+package days
+
+data class Crossroad(val left: String, val right: String)
+
+fun day8(input: List<String>): Int {
+    val instructions = input[0]
+    val graph = mutableMapOf<String, Crossroad>()
+    input.forEachIndexed { i, line ->
+        if (i < 2) return@forEachIndexed
+        val (current, next) = line.split(" = (")
+        var (left, right) = next.split(", ")
+        right = right.replace(")", "")
+        graph[current] = Crossroad(left, right)
+    }
+    var i = 0
+    var current = "AAA"
+    while (true) {
+        val direction = instructions[i % instructions.length]
+        current = if (direction == 'L') graph[current]!!.left else graph[current]!!.right
+        i++
+        if (current == "ZZZ")
+            break
+    }
+    return i
+}
+
+fun day82(input: List<String>): Long {
+    val instructions = input[0]
+    val graph = mutableMapOf<String, Crossroad>()
+    val startNodes = mutableListOf<String>()
+    input.forEachIndexed { i, line ->
+        if (i < 2) return@forEachIndexed
+        val (current, next) = line.split(" = (")
+        if (current.last() == 'A')
+            startNodes.add(current)
+
+        var (left, right) = next.split(", ")
+        right = right.replace(")", "")
+        graph[current] = Crossroad(left, right)
+    }
+    System.err.println("${startNodes.size} nodes needed")
+    for (n in 0 until startNodes.size) {
+        var currentNodes = startNodes.toList()
+        System.err.println("Searching for endnodes for node $n")
+        var endnodes = 0
+        var maxNrFound = 0
+        var i = 0L
+        while (true) {
+            val direction = instructions[(i % instructions.length).toInt()]
+            val newNodes = mutableListOf<String>()
+            currentNodes.forEach {
+                newNodes.add(if (direction == 'L') graph[it]!!.left else graph[it]!!.right)
+            }
+            i++
+            if (i % 10_000_000 == 0L) {
+                System.err.println(i)
+            }
+            newNodes[n].let {
+                if (it.last() == 'Z')
+                    endnodes++
+            }
+            if (endnodes > maxNrFound) {
+                System.err.println("$endnodes found at step $i")
+                maxNrFound = endnodes
+            }
+            if (endnodes == newNodes.size/2)
+                break
+            currentNodes = newNodes
+        }
+    }
+    return 0
+}
+
+fun main() {
+    val input = readFileLines("_2023/inputFiles/Day8")
+    require(input.isNotEmpty()) { "Input file must not be empty" }
+    //println(day8(input))
+    println(day82(input))
+}
---
+++ _2023/test/Day8Test.kt
@@ -0,0 +1,49 @@
+import days.day8
+import days.day82
+import junit.framework.TestCase
+
+class Day8KtTest : TestCase() {
+    val input = """
+        RL
+
+        AAA = (BBB, CCC)
+        BBB = (DDD, EEE)
+        CCC = (ZZZ, GGG)
+        DDD = (DDD, DDD)
+        EEE = (EEE, EEE)
+        GGG = (GGG, GGG)
+        ZZZ = (ZZZ, ZZZ)
+    """.trimIndent().lines()
+
+    val input2 = """
+LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)
+    """.trimIndent().lines()
+
+    fun test1day8() {
+        val ans = day8(input)
+        assertEquals(2, ans)
+        val ans2 = day8(input2)
+        assertEquals(6, ans2)
+    }
+
+    val input3 = """
+        LR
+
+        11A = (11B, XXX)
+        11B = (XXX, 11Z)
+        11Z = (11B, XXX)
+        22A = (22B, XXX)
+        22B = (22C, 22C)
+        22C = (22Z, 22Z)
+        22Z = (22B, 22B)
+        XXX = (XXX, XXX)
+    """.trimIndent().lines()
+    fun test2day8() {
+        val ans = day82(input3)
+        assertEquals(6, ans)
+    }
+}
